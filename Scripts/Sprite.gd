bc88ad43a2ed8966565a89a0c0e1cce6bcd907de
GreatArgorath <adam.j.baker@gmail.com>
2020-01-27 21:10:13 +0000

    Added test sprite for Github

---
+++ Scripts/Sprite.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("This is a test for Github")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
