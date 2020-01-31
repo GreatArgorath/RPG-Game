bde103ff30d5738e87ef2e26b6b72b59cbbbc575
GreatArgorath <adam.j.baker@gmail.com>
2020-01-31 23:14:46 +0000

    Added grass variant

--- Scenes/Gun.gd
+++ Scenes/Gun.gd
@@ -5,5 +5,7 @@ var gunRotation = get_transform().get_rotation()
 func _process(delta):
 	var mpos = get_global_mouse_position()
 	look_at(mpos)
-	if gunRotation > 90:
+	if gunRotation > -1.5 and gunRotation < 1.5:
 		set_flip_v(true)
+	else:
+		set_flip_v(false)
