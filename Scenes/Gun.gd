b7613184768e75277850a6350e25fb3c9069e245
GreatArgorath <adam.j.baker@gmail.com>
2020-02-01 18:56:29 +0000

    Updated textures and main area (v0.12)

--- Scenes/Gun.gd
+++ Scenes/Gun.gd
@@ -1,11 +1,9 @@
 extends Sprite
 
-var gunRotation = get_transform().get_rotation()
+var gunRotation = get_rotation()
 
 func _process(delta):
 	var mpos = get_global_mouse_position()
 	look_at(mpos)
-	if gunRotation > -1.5 and gunRotation < 1.5:
-		set_flip_v(true)
-	else:
-		set_flip_v(false)
+	print(gunRotation)
+
